7abb4dba9efa737b529ec321c674ee8708f62daf
palexv <palexvtrade@gmail.com>
2025-10-20 21:26:26 +0300

    Изменил файл урок 2 задача 2

--- lesson2_task2.py
+++ lesson2_task2.py
@@ -1,1 +1,10 @@
-print("Урок 2 задача 2")
+chislo = int(input("Введите оценку от 1 до 10 "))
+
+if chislo <= 10 and chislo >= 9:
+    print("Отлично!")
+elif chislo == 8 or chislo == 7:
+    print("Хорошо")
+elif chislo < 7 and chislo > 5:
+    print("Удовлетворительно")
+else:
+    print("Неудовлетворительно")
