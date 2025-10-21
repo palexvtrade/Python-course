2161578791b2de0a053d9983fd3ea8ce296789a2
palexvtrade <palexvtrade@gmail.com>
2025-10-21 06:29:51 +0300

    Добавил условие в урок 2 задача 2

--- lesson2_task2.py
+++ lesson2_task2.py
@@ -6,5 +6,7 @@ elif chislo == 8 or chislo == 7:
     print("Хорошо")
 elif chislo < 7 and chislo > 5:
     print("Удовлетворительно")
-else:
+elif chislo <= 5 and chislo >=1:
     print("Неудовлетворительно")
+else:
+    print("Ваша оценка некорректна, за пределами диапазона от 1 до 10!!!")
